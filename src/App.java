c1d6179a16d3e19cadcbe9927363897561cd54e5
C.Fulton <cfulton@luc.edu>
2026-02-04 19:12:37 -0600

    adding array and looping structure to output names

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        String [] folks = {"Logan", "Cui", "Bilal"};
         System.out.println("Hello, Class!");
+
+        for(var i = 0; i < folks.length; i++){
+            System.out.print(folks[i] + " ");
+        }
     }
 }
