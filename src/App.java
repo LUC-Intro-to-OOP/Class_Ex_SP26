d978ab523df71d9ab1a44f9dcb1539c4a5a274c2
C.Fulton <cfulton@luc.edu>
2026-02-11 19:46:46 -0600

    adding new item array along price

--- src/App.java
+++ src/App.java
@@ -2,15 +2,15 @@ public class App {
     public static void main(String[] args) throws Exception {
 
 
-        String [] groceries = {"Eggs", "Fruit", "Tim Tams"}; //Collection Initializer
-        double [] prices = {13.00, 3.00, 6.50};
+        String [] groceries = {"Eggs", "Fruit", "Tim Tams", "Yams"}; //Collection Initializer
+        double [] prices = {13.00, 3.00, 6.50, 10.00};
         String foundItem = "";
         double foundPrice =0.00;
         boolean isFound = false; //flag variable
         for(var i = 0; i < groceries.length; i++){
             //System.out.print(groceries[i] + " ");
             //Selection structure
-            if(groceries[i].equalsIgnoreCase("Fruit")){
+            if(groceries[i].equalsIgnoreCase("Yams")){
                 isFound = true;
                 
                 foundItem = groceries[i];
