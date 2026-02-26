de8b43cfaee92b5cd99cc6c4690119d49c7289f8
C.Fulton <cfulton@luc.edu>
2026-02-25 20:24:10 -0600

    adding additional oop code to example

--- oop_intro/src/ApartmentManager.java
+++ oop_intro/src/ApartmentManager.java
@@ -1,24 +1,53 @@
+import java.util.*;
+
+
 public class ApartmentManager {
     public static void main(String[] args) throws Exception {
-      Apartment aparment_one;
+      
 
         //Declarations
-        Renter renter_one = new Renter();//Instantiated a new Renter object
-        renter_one.name = "Caden";
-        renter_one.address = "123 Fulton Blvd";
-        renter_one.numberOfBathrooms = 2;
-        renter_one.numberOfBedrooms = 3;
+        Scanner input = new Scanner(System.in);
+        //[] renterList = new Renter[3]; //Array that is going hold objects
+
+        //ArrayList that will hold my renters
+        ArrayList<Renter> list = new ArrayList<>();
+
+
+
+        Renter aRenter; //Declaring a renter object
+        String name;
+
+
+        //Create a looping structure that is going to iterate and populate my array
+        for(var x = 0; x < 2; x++){
+          System.out.print("Please enter name number" + (x+1) + ":");
+          name = input.nextLine();
+          aRenter = new Renter(name); //Instantiate the object
+          list.add(aRenter);//adding the newly created object instance to the list
+          //renterList[x] = new Renter(name); //istantiated my object instance and populated my array
+
+        }
+
+        for(var i = 0; i < 2; i++){
+          System.out.print(list.get(i));
+        }
        
 
 
+   
+       /*  
+        Renter renter_one = new Renter("Caden", "123 Fulton Blvd", 2,2);//Instantiated a new Renter object
+        Renter renterCui = new Renter("Cui");
         Renter renter_two = new Renter();
-        renter_two.name = "Bilal";
-        renter_two.address = "125 Futon Blvd";
-        renter_two.numberOfBathrooms = 4;
-        renter_two.numberOfBedrooms = 1;
+        renter_two.setName("Bilal");
+        renter_two.setAddress("125 Futon Blvd");
+        renter_two.setBathrooms(4);;
+        renter_two.setBedRoom(1);
 
-        System.out.println("Renter One: " + renter_one);
+        //System.out.println("Renter One: " + renter_one);
+        System.out.println("Name: " + renter_one.getName() + " Address: " + renter_one.getAddress());
         System.out.println("Renter Two: " + renter_two);
-        
+        System.out.println(renterCui);
+        */
     }
 }
--- oop_intro/src/Renter.java
+++ oop_intro/src/Renter.java
@@ -1,12 +1,66 @@
 public class Renter {
     
     //Attributes or Properties
-    String name;
-    String address;
-    int numberOfBedrooms;
-    int numberOfBathrooms;
+    private String name;
+    private String address;
+    private int numberOfBedrooms;
+    private int numberOfBathrooms;
+
+    //Default Constructor
+    public Renter(){
+        name = "N/A";
+        address = "address";
+        numberOfBedrooms = 0;
+        numberOfBathrooms = 0;
+    }
 
 
+    //Overloading the Constructor
+    public Renter(String name, String address, int bedCount, int bathCount){
+        this.name = name;
+        this.address = address;
+        numberOfBedrooms = bedCount;
+        numberOfBathrooms = bathCount;
+    } 
+
+    //Overloaded construction that accepts one parameter
+    public Renter(String name){
+        this.name = name;
+        address = "address";
+        numberOfBedrooms = 0;
+        numberOfBathrooms = 0;
+    }
+
+    //Getter and Setter methods
+    public void setName(String aName){
+        name = aName;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public void setBedRoom(int roomCount){
+        numberOfBedrooms = roomCount;
+    }
+    public void setBathrooms(int bathCount){
+        numberOfBathrooms = bathCount;
+    }
+
+    //Getter Methods
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public int getRoomCount(){
+        return numberOfBedrooms;
+    }
+
+    public int getBathCount(){
+        return numberOfBathrooms;
+    }
 
     public String toString(){
 
